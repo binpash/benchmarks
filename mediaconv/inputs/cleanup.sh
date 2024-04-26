0c03bd1b1dd160cea3ef3251b8c233dae34b7152
Nikos Pagonas <nikospagonas00@gmail.com>
2024-04-26 11:35:01 -0400

    Set up cleanup
    This might not be what we want, but I can't find the inputs
    (the pac MIT machine is down), so I can't run the script
    in order to figure out what the intermediate inputs should be.

---
+++ mediaconv/inputs/cleanup.sh
@@ -0,0 +1,17 @@
+IN=$PASH_TOP/evaluation/benchmarks/dependency_untangling/input/
+OUT=$PASH_TOP/evaluation/benchmarks/dependency_untangling/output/
+
+rm -rf ${IN}/jpg
+rm -rf ${IN}/log_data
+rm -rf ${IN}/wav
+rm -rf ${IN}/nginx-logs
+rm -rf ${IN}/node_modules
+rm -rf ${IN}/pcap_data
+rm -rf ${IN}/pcaps
+rm -rf ${IN}/packages
+rm -rf ${IN}/mir-sa
+rm -rf ${IN}/deps
+rm -rf ${IN}/bio
+rm -rf ${IN}/output
+rm -rf ${OUT}
+exit

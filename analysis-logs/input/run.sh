968b344d0625f1e2e885e35a79c75a7daa2dfd08
gliargovas <gliargovas@aueb.gr>
2024-05-08 01:30:57 +0000

    Add complete analysis-logs benchmark

---
+++ analysis-logs/input/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+REPO_TOP=$(git rev-parse --show-toplevel)
+
+eval_dir="${REPO_TOP}/analysis-logs"
+results_dir="${eval_dir}/results"
+inputs_dir="${eval_dir}/input"
+
+shell="/bin/bash"
+
+mkdir -p $results_dir
+
+export INPUT=${inputs_dir}/access.log
+script="${eval_dir}/nginx.sh"
+echo $(basename "$script")
+echo "$results_dir/$(basename "$script").out"
+
+echo "Executing nginx.sh"
+$shell "$script" > "$results_dir/$(basename "$script").out"

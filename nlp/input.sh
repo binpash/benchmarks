eb1d982dc7350ff220f49d6a8b089ed7d60a352e
Zhicheng Huang <skyhuang@umich.edu>
2024-05-17 02:12:29 -0400

    Pulled books resources from atlas server

--- nlp/input.sh
+++ nlp/input.sh
@@ -18,12 +18,25 @@ append_nl_if_not() {
     fi
 }
 
+BOOKS_URL="https://atlas-group.cs.brown.edu/data/gutenberg/books.txt"
+BASE_URL="https://atlas-group.cs.brown.edu/data/gutenberg/"
 
+# Download the list of book urls
+if [ ! -f ./book_links.txt ]; then
+    wget -q -O book_links.txt "$BOOKS_URL"
+    if [ ! -f book_links.txt ]; then
+        echo "Failed to download book_links.txt"
+        exit 1
+    fi
+fi
+
+# not specified on atlas server, so still using original link
 if [ ! -f ./genesis ]; then
     curl -sf https://www.gutenberg.org/cache/epub/8001/pg8001.txt > genesis
     append_nl_if_not genesis
 fi 
 
+# not specified on atlas server, so still using original link
 if [ ! -f ./exodus ]; then
     curl -sf https://www.gutenberg.org/files/33420/33420-0.txt > exodus
     append_nl_if_not exodus
@@ -32,23 +45,16 @@ fi
 if [ ! -e ./pg ]; then
     mkdir pg
     cd pg
-    book_count=10
+    book_count=120
     if [[ "$1" == "--full" ]]; then
         book_count=1000
     fi
 
-    echo $PWD
-
-    if [ ! -f ../book_txt_links.txt ]; then 
-        echo "No link file found!" 
-        exit 1
-    fi
-
-    head -n $book_count ../book_txt_links.txt | while IFS= read -r line
+    head -n $book_count ../book_links.txt | while IFS= read -r line
     do
-        echo "Downloading $line"
-        # Your code here
-        wget -q "$line"
+        full_url="${BASE_URL}${line}"
+        echo "Downloading $full_url"
+        wget -q "$full_url"
     done
 
     for f in *.txt; do

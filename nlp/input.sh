b785a8dac545f9b00587021c4b9b36ecc7d9e381
Zhicheng Huang <skyhuang@umich.edu>
2024-05-17 02:42:13 -0400

    Updated genesis and exodus links to atlas and reverted default book_count

--- nlp/input.sh
+++ nlp/input.sh
@@ -30,22 +30,22 @@ if [ ! -f ./book_links.txt ]; then
     fi
 fi
 
-# not specified on atlas server, so still using original link
 if [ ! -f ./genesis ]; then
-    curl -sf https://www.gutenberg.org/cache/epub/8001/pg8001.txt > genesis
-    append_nl_if_not genesis
+    # original link: https://www.gutenberg.org/cache/epub/8001/pg8001.txt
+    curl -sf https://atlas-group.cs.brown.edu/data/gutenberg/8/0/0/8001/8001.txt > genesis
+    "$PASH_TOP/scripts/append_nl_if_not.sh" genesis
 fi 
 
-# not specified on atlas server, so still using original link
 if [ ! -f ./exodus ]; then
-    curl -sf https://www.gutenberg.org/files/33420/33420-0.txt > exodus
-    append_nl_if_not exodus
+    # original link: https://www.gutenberg.org/files/33420/33420-0.txt
+    curl -sf https://atlas-group.cs.brown.edu/data/gutenberg/3/3/4/2/33420/33420-0.txt > exodus
+    "$PASH_TOP/scripts/append_nl_if_not.sh" exodus
 fi
 
 if [ ! -e ./pg ]; then
     mkdir pg
     cd pg
-    book_count=120
+    book_count=10
     if [[ "$1" == "--full" ]]; then
         book_count=1000
     fi

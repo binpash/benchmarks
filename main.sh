255866ba118e7c821ac19377c952b2dbbe1c39eb
Evangelos Lamprou <vagos@lamprou.xyz>
2024-10-20 18:58:58 -0400

    Fix main.sh

--- main.sh
+++ main.sh
@@ -1,21 +1,36 @@
-BENCHMARK="$1"
-
-cd "$(dirname "$0")/$BENCHMARK" || exit 1
-
 error()
 {
     echo "Error: $1" > /dev/stderr
     exit 1
 }
 
-# Download dependencies
-./deps.sh || error "Failed to download dependencies for $BENCHMARK"
+correct() { [ "$(cat $BENCHMARK.hash | cut -d' ' -f 2 | grep -c 1)" -eq 0 ]; }
 
-# Fetch inputs
-./input.sh || error "Failed to fetch inputs for $BENCHMARK"
+main()
+{
+    cd "$(dirname "$0")/$BENCHMARK" || exit 1
+    # Download dependencies
+    ./deps.sh $@ || error "Failed to download dependencies for $BENCHMARK"
 
-# Run benchmark
-./run.sh > $BENCHMARK.out 2> $BENCHMARK.err || error "Failed to run $BENCHMARK"
+    # Fetch inputs
+    ./inputs.sh $@  || error "Failed to fetch inputs for $BENCHMARK"
 
-# Verify output
-./verify.sh > $BENCHMARK.hash || error "Failed to verify output for $BENCHMARK"
+    # Run benchmark
+    ( ./run.sh $@ > $BENCHMARK.out 2> $BENCHMARK.err ) || error "Failed to run $BENCHMARK"
+
+    # Verify output
+    ./verify.sh $@ > $BENCHMARK.hash || error "Failed to verify output for $BENCHMARK"
+
+    if correct; then
+        echo "$BENCHMARK [pass]"
+    else
+        error "$BENCHMARK [fail]"
+    fi
+
+    cd - || exit 1
+}
+
+export BENCHMARK="$1"
+shift
+
+main $@

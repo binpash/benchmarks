a12f4f979b5e42ea49b4569257bb3d888feb5565
Evangelos Lamprou <vagos@lamprou.xyz>
2024-10-20 23:05:20 -0400

    Make file-env verify follow format

--- file-enc/verify.sh
+++ file-enc/verify.sh
@@ -6,25 +6,28 @@
 #md5sum compress_files.full/* > /benchmarks/file-enc/hashes/compress_files.small.md5sum
 #md5sum encrypt_files.full/* > /benchmarks/file-enc/hashes/encrypt_files.small.md5sum
 
-REPO_TOP=$(git rev-parse --show-toplevel)
-eval_dir="${REPO_TOP}/file-enc"
-results_dir="${eval_dir}/results"
-hashes_dir="${eval_dir}/hashes"
+cd "$(realpath $(dirname "$0"))"
+
+results_dir="results"
+hashes_dir="hashes"
 
 suffix=".full"
-if [[ $1 == "--small" ]]; then
+if [[ $@ == *"--small"* ]]; then
     suffix=".small"
 fi
 
-cd $results_dir # md5sum computes paths relative to cd
-okay=0
-if ! md5sum --check --quiet $hashes_dir/encrypt_files$suffix.md5sum; then
-    okay=1
-    echo "encrypt_files $suffix failed verification"
-fi
-if ! md5sum --check --quiet $hashes_dir/compress_files$suffix.md5sum; then
-    okay=1
-    echo "compress_files $suffix failed verification"
+if [[ $@ == *"--generate"* ]]; then
+    md5sum $results_dir/compress_files$suffix/* > "$hashes_dir/compress_files$suffix.md5sum"
+    md5sum $results_dir/encrypt_files$suffix/* > "$hashes_dir/encrypt_files$suffix.md5sum"
+    exit 0
 fi
 
-exit $okay
+okay=0
+if ! md5sum --check --quiet "$hashes_dir/encrypt_files$suffix.md5sum"; then
+    okay=1
+    echo "encrypt_files $okay"
+fi
+if ! md5sum --check --quiet "$hashes_dir/compress_files$suffix.md5sum"; then
+    okay=1
+    echo "compress_files $okay"
+fi

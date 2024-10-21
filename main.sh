bcc97fe8a240b4c3ce703c563cd2be3a2e8360dd
Evangelos Lamprou <vagos@lamprou.xyz>
2024-10-20 23:12:07 -0400

    Fix main.sh
    * Rename inputs to input
    * Add cleanup
    * Formatting

--- main.sh
+++ main.sh
@@ -9,17 +9,21 @@ correct() { [ "$(cat $BENCHMARK.hash | cut -d' ' -f 2 | grep -c 1)" -eq 0 ]; }
 main()
 {
     cd "$(dirname "$0")/$BENCHMARK" || exit 1
+
     # Download dependencies
     ./deps.sh $@ || error "Failed to download dependencies for $BENCHMARK"
 
     # Fetch inputs
-    ./inputs.sh $@  || error "Failed to fetch inputs for $BENCHMARK"
+    ./input.sh $@  || error "Failed to fetch inputs for $BENCHMARK"
 
     # Run benchmark
     ( ./run.sh $@ > $BENCHMARK.out 2> $BENCHMARK.err ) || error "Failed to run $BENCHMARK"
 
     # Verify output
     ./verify.sh $@ > $BENCHMARK.hash || error "Failed to verify output for $BENCHMARK"
+
+    # Cleanup outputs
+    ./cleanup.sh $@
 
     if correct; then
         echo "$BENCHMARK [pass]"

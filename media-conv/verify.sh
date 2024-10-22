043e7befe14a83f9cc43dd60ce68041cb3a9d918
EtomicBomb <ethan@ethan.ws>
2024-10-22 15:11:38 -0400

    changed verify script

--- media-conv/verify.sh
+++ media-conv/verify.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+hash_audio_dir() {
+    local src_dir=$1
+    for src in $src_dir/*; do
+        got_hash=$(ffmpeg -i "$src" -map 0:a -f md5 - 2>/dev/null)
+        echo $got_hash $(realpath "--relative-to=$src_dir" "$src")
+    done
+}
+
 REPO_TOP=$(git rev-parse --show-toplevel)
 eval_dir="${REPO_TOP}/media-conv"
 results_dir="${eval_dir}/results"
@@ -10,18 +18,20 @@ if [[ "$@" == *"--small"* ]]; then
     suffix=".small"
 fi
 
-cd $results_dir # md5sum computes paths relative to cd
-
 if [[ "$@" == *"--generate"* ]]; then
-    md5sum img_convert$suffix/* > $hashes_dir/img_convert$suffix.md5sum
-    md5sum to_mp3$suffix/* > $hashes_dir/to_mp3$suffix.md5sum
+    bench=img_convert$suffix
+    md5sum $results_dir/$bench/* > "$hashes_dir/$bench.md5sum"
+
+    bench=to_mp3$suffix
+    hash_audio_dir "$results_dir/$bench" > "$hashes_dir/$bench.md5sum"
     exit 0
 fi
+
 
 bench=img_convert$suffix
 md5sum --check --quiet --status $hashes_dir/$bench.md5sum
 echo $bench $?
 
 bench=to_mp3$suffix
-md5sum --check --quiet --status $hashes_dir/$bench.md5sum
+hash_audio_dir "$results_dir/$bench" | diff -q "$hashes_dir/$bench.md5sum" -
 echo $bench $?

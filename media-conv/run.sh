204ca5536206cf257f6ccb3d4497239a003ca862
Evangelos Lamprou <vagos@lamprou.xyz>
2024-10-21 21:16:34 -0400

    Add output messages

--- media-conv/run.sh
+++ media-conv/run.sh
@@ -17,5 +17,10 @@ if [[ "$@" == *"--small"* ]]; then
     suffix=".small"
 fi
 
-$scripts_dir/img_convert.sh $img_convert_input $results_dir/img_convert$suffix > $results_dir/img_convert$suffix.log
-$scripts_dir/to_mp3.sh $to_mp3_input $results_dir/to_mp3$suffix > $results_dir/to_mp3$suffix.log
+echo "img_convert"
+time $scripts_dir/img_convert.sh $img_convert_input $results_dir/img_convert$suffix > $results_dir/img_convert$suffix.log
+echo $?
+
+echo "to_mp3"
+time $scripts_dir/to_mp3.sh $to_mp3_input $results_dir/to_mp3$suffix > $results_dir/to_mp3$suffix.log
+echo $?

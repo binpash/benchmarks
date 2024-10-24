112e0c7dc29e41e841a6d8f2f69a0d75f62f0d49
EtomicBomb <ethan@ethan.ws>
2024-10-24 14:03:52 -0400

    work on covid-mts

--- covid-mts/run.sh
+++ covid-mts/run.sh
@@ -1,47 +1,22 @@
 #!/bin/bash
 
-export SUITE_DIR=$(realpath $(dirname "$0"))
-export TIMEFORMAT=%R
-cd $SUITE_DIR
+REPO_TOP=$(git rev-parse --show-toplevel)
+eval_dir="${REPO_TOP}/covid-mts"
+input_dir="${eval_dir}/input"
+outputs_dir="${eval_dir}/outputs"
+scripts_dir="${eval_dir}/scripts"
 
-if [[ "$1" == "--small" ]]; then
-    echo "Using small input"
-    input_file="$SUITE_DIR/inputs/in_small.csv"
-else
-    echo "Using default input"
-    input_file="$SUITE_DIR/inputs/in.csv"
+suffix=""
+if [[ "$@" == *"--small"* ]]; then
+    suffix="_small"
 fi
 
-mkdir -p "outputs"
-all_res_file="./outputs/covid-mts.res"
-> $all_res_file
+input_file="$input_dir/in$suffix.csv"
+output_scoped="$outputs_dir/outputs$suffix"
+mkdir -p "$output_scoped"
 
-# time_file stores the time taken for each script
-# mode_res_file stores the time taken and the script name for every script in a mode (e.g. bash, pash, dish, fish)
-# all_res_file stores the time taken for each script for every script run, making it easy to copy and paste into the spreadsheet
-covid-mts() {
-    mkdir -p "outputs/$1"
-    mode_res_file="./outputs/$1/covid-mts.res"
-    > $mode_res_file
+"$scripts_dir/1.sh" "$input_file" > "$output_scoped/1.out"
+"$scripts_dir/2.sh" "$input_file" > "$output_scoped/2.out"
+"$scripts_dir/3.sh" "$input_file" > "$output_scoped/3.out"
+"$scripts_dir/4.sh" "$input_file" > "$output_scoped/4.out"
 
-    echo executing covid-mts $1 $(date) | tee -a $mode_res_file $all_res_file
-
-    for number in `seq 4` ## initial: FIXME 5.sh is not working yet
-    do
-        script="${number}"
-        script_file="./scripts/$script.sh"
-        output_dir="./outputs/$1/$script/"
-        output_file="./outputs/$1/$script.out"
-        time_file="./outputs/$1/$script.time"
-        log_file="./outputs/$1/$script.log"
-
-        if [[ "$1" == "bash" ]]; then
-            (time bash $script_file $input_file > $output_file ) 2> $time_file
-        fi
-
-        cat "${time_file}" >> $all_res_file
-        echo "$script_file $(cat "$time_file")" | tee -a $mode_res_file
-    done
-}
-
-covid-mts "bash"

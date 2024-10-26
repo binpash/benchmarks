5b8d78d4b5e7c70c5e8b5541b732413718a6d5fd
Zhuoxuan Zhang <zz3052@columbia.edu>
2024-10-26 00:16:09 -0400

    test

--- oneliners/verify.sh
+++ oneliners/verify.sh
@@ -56,9 +56,6 @@ do
             echo "$hash" > "$folder/$filename.hash"
         fi
 
-        # Compare the hash with the hash in the hashes directory
-        diff "$hash_folder/$filename.hash" "$folder/$filename.hash"
-
         # Print the filename and hash
         echo "File: $folder/$filename | SHA-256 Hash: $(cat "$folder/$filename.hash")"
     done

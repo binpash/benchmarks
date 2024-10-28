3c989827c4a30867a2e04c314f80fbcbdfcec72a
EtomicBomb <ethan@ethan.ws>
2024-10-28 19:33:05 -0400

    working on vim and sqlite

---
+++ riker/scripts/sqlite/deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo echo "Europe/London" > /etc/timezone 
+sudo dpkg-reconfigure -f noninteractive tzdata \
+sudo apt-get install -y git \
+    gcc \
+    tcl8.6 \
+    libtool \
+    libtool-bin \
+    libreadline-dev \
+    tcl8.6-dev
+

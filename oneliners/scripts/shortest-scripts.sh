b825aa90933edbbd3302ea31806f4e63daa04a81
Zhuoxuan Zhang <hzhang6@oberlin.edu>
2024-10-28 00:04:09 -0400

    Update shortest-scripts.hash and shortest-scripts.sh
    Update the hash in shortest-scripts.hash from 7f4e10480a04ced2b1545fbfbef225f707efa229096afae602b743657ad79fd4 to 83f794834d004114913d1e4f3e480270a0af0e7a30ebd2948e82187a9a3b9e0f.
    
    In shortest-scripts.sh, fix the command to sort the output numerically and by the second column. Also, add a missing newline at the end of the file.
    
    In verify.sh, add a line to compare the hash in the hash folder with the hash in the current folder.

--- oneliners/scripts/shortest-scripts.sh
+++ oneliners/scripts/shortest-scripts.sh
@@ -4,6 +4,4 @@
 # +p.95 multiple sed
 # +p.XX crawler
 
-# FIX: Input here should be a set of commands, more precisely, the ones on this specific machine.
-
-cat $1 | xargs file | grep "shell script" | cut -d: -f1 | xargs -L 1 wc -l | grep -v '^0$' | sort -n | head -15
+cat "$1" | xargs file | grep "shell script" | cut -d: -f1 | xargs -L 1 wc -l | grep -v '^0$' | sort -n -k1,1 -k2 | head -15

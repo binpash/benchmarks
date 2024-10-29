ef03f8b0110b540c1247dbebf11c4c5b4931a9f7
Zhuoxuan-Zhang <zz3052@columbia.edu>
2024-10-29 13:35:02 -0400

    no echos for verify.sh

--- web-index/verify.sh
+++ web-index/verify.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 REPO_TOP=$(git rev-parse --show-toplevel)
-echo "REPO_TOP: ${REPO_TOP}"
 eval_dir="${REPO_TOP}/web-index"
 hashes_dir="${eval_dir}/hashes"
 
@@ -27,7 +26,6 @@ fi
 
 # verify hashes for all *grams.txt files
 for file in $(find ${eval_dir} -name "*grams.txt"); do
-    echo "Verifying hash for ${file}"
     hash=$(sha256sum ${file} | cut -d ' ' -f 1)
     expected_hash=$(cat "${hashes_dir}/$(basename ${file})${suffix}.hash")
     if [[ "${hash}" != "${expected_hash}" ]]; then
@@ -36,6 +34,5 @@ for file in $(find ${eval_dir} -name "*grams.txt"); do
         echo "Actual: ${hash}"
         exit 1
     fi
-    echo "Hash verified for ${file}"
 done
-echo "web-index $?"
+echo $?

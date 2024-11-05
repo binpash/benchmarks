6198cf1f8555a83aae49686c9a25cd06688d860c
EtomicBomb <ethan@ethan.ws>
2024-11-05 15:39:17 -0500

    clarify vim verification script

--- riker/scripts/vim/verify.sh
+++ riker/scripts/vim/verify.sh
@@ -5,9 +5,17 @@ eval_dir="${REPO_TOP}/riker"
 scripts_dir="${eval_dir}/scripts"
 input_dir="${eval_dir}/input"
 
+# Call compiled binary to write an empty file with a randomly chosen path.
+# Must use -u /dev/null to specify an empty config because there might not be a ~/.vimrc, which vim would complain about.
+# Assert that the file was created.
+
 vim="$input_dir/scripts/vim/dev/src/vim"
 canary=$(mktemp --dry-run --tmpdir="$input_dir/scripts/vim")
 
+test ! -f $canary
+echo riker/vim/canary-did-not-exist $?
+
 "$vim" -u /dev/null -e -c "w $canary" -c quit
+
 test -f $canary
-echo riker/vim $?
+echo riker/vim/created-canary-file $?

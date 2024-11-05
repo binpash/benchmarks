68dec2484022c34af1fbd576ab2ac76c98535e8d
EtomicBomb <ethan@ethan.ws>
2024-11-05 18:49:39 -0500

    fixup message in vim

--- riker/scripts/vim/verify.sh
+++ riker/scripts/vim/verify.sh
@@ -6,16 +6,16 @@ scripts_dir="${eval_dir}/scripts"
 input_dir="${eval_dir}/input"
 
 # Call compiled binary to write an empty file with a randomly chosen path.
-# Must use -u /dev/null to specify an empty config because there might not be a ~/.vimrc, which vim would complain about.
+# Must use -u /dev/null to specify a blank config because there might not be a ~/.vimrc, which vim would complain about.
 # Assert that the file was created.
 
 vim="$input_dir/scripts/vim/dev/src/vim"
-canary=$(mktemp --dry-run --tmpdir="$input_dir/scripts/vim")
+canary="$(mktemp --dry-run --tmpdir="$input_dir/scripts/vim")"
 
-test ! -f $canary
+test ! -f "$canary"
 echo riker/vim/canary-did-not-exist $?
 
 "$vim" -u /dev/null -e -c "w $canary" -c quit
 
-test -f $canary
+test -f "$canary"
 echo riker/vim/created-canary-file $?

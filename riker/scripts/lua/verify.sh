7a429a6add9c8db8c02b7e4671ee1b12a98828c0
EtomicBomb <ethan@ethan.ws>
2024-11-05 17:56:33 -0500

    added verification for lua

--- riker/scripts/lua/verify.sh
+++ riker/scripts/lua/verify.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-# No known verify script
+REPO_TOP="$(git rev-parse --show-toplevel)"
+eval_dir="${REPO_TOP}/riker"
+scripts_dir="${eval_dir}/scripts"
+input_dir="${eval_dir}/input"
+
+"$input_dir/scripts/lua/lua-5.4.3/src/lua" -e 'print("Hello, Riker!")' | diff -q - <(echo "Hello, Riker!")
+echo riker/lua $?

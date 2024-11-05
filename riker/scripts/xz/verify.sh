36ed2d83500fcff8e99e3898f3ee04b7d51cc6ce
EtomicBomb <ethan@ethan.ws>
2024-11-05 18:49:23 -0500

    add verification for xz

--- riker/scripts/xz/verify.sh
+++ riker/scripts/xz/verify.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-# no verification at this time
+REPO_TOP="$(git rev-parse --show-toplevel)"
+eval_dir="${REPO_TOP}/riker"
+scripts_dir="${eval_dir}/scripts"
+input_dir="${eval_dir}/input"
+
+xz_bin="$input_dir/scripts/xz/dev/xz"
+
+echo "Hello, Riker!" \
+    | "$xz_bin" \
+    | "$xz_bin" -d \
+    | diff -q - <(echo Hello, Riker!)
+echo riker/xz $?

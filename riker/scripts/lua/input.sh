72dcbde13b6376f7f44b072b1405abecf1332a16
EtomicBomb <ethan@ethan.ws>
2024-11-05 19:10:18 -0500

    add mkdir to lua verification

--- riker/scripts/lua/input.sh
+++ riker/scripts/lua/input.sh
@@ -4,6 +4,8 @@ REPO_TOP="$(git rev-parse --show-toplevel)"
 eval_dir="${REPO_TOP}/riker"
 input_dir="${eval_dir}/input/scripts/lua"
 
+mkdir -p "$input_dir"
+
 tar_path="$input_dir/lua-5.4.3.tar.gz"
 wget https://www.lua.org/ftp/lua-5.4.3.tar.gz -O "$tar_path"
 tar xzf "$tar_path" -C "$input_dir"

60e934d3dd50d7e36bf98e1e0e9dab0fe3538efe
Zhuoxuan Zhang <hzhang6@oberlin.edu>
2024-11-25 13:53:47 -0500

    okay aurpkg

--- aurpkg/cleanup.sh
+++ aurpkg/cleanup.sh
@@ -1,7 +1,5 @@
-IN=$PASH_TOP/evaluation/benchmarks/dependency_untangling/input/
-OUT=$PASH_TOP/evaluation/benchmarks/dependency_untangling/output/
-IN_NAME=$PASH_TOP/evaluation/benchmarks/dependency_untangling/input/100G.txt
-
+IN=input/
+OUT=outputs/
 
 rm -rf ${IN}/packages
 rm -rf ${OUT}

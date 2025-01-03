11d0ae96a798c4c15b84b09bae874249c8a122cd
EtomicBomb <ethan@ethan.ws>
2025-01-03 03:07:28 -0500

    working on remedying an issue

--- infrastructure/run_dynamic_shell.py
+++ infrastructure/run_dynamic_shell.py
@@ -17,7 +17,7 @@ from all_scripts import get_all_scripts
 from syntax_analysis import parse_shell_script, count_nodes
 from project_root import get_project_root
 
-def data_json(p: psutil.Process, log_current_time: str, benchmark_experiment_start: str) -> str: 
+def data_json(p: psutil.Process, log_current_time: str, benchmark_experiment_start: str, benchmark_script: Path) -> str: 
     parent_pid = None
     try:
         parent_pid = p.parent().pid
@@ -31,7 +31,7 @@ def data_json(p: psutil.Process, log_current_time: str, benchmark_experiment_sta
         'pid': p['pid'],
         'parent': parent_pid,
         'benchmark_category': p['environ'].get('BENCHMARK_CATEGORY'),
-        'benchmark_script': p['environ'].get('BENCHMARK_SCRIPT'),
+        'benchmark_script': str(benchmark_script),
         'benchmark_input_file': p['environ'].get('BENCHMARK_INPUT_FILE'),
         'benchmark_experiment_start': benchmark_experiment_start,
         'log_current_time': log_current_time,
@@ -55,35 +55,37 @@ def data_json(p: psutil.Process, log_current_time: str, benchmark_experiment_sta
         'full': p, # this does not provide field names for cpu_times and io_counters, etc.
     })
 
-def write_process_data(parent: int, data_log, benchmark_experiment_start):
+def write_process_data(parent: int, data_log, benchmark_experiment_start, benchmark_script: Path):
     log_current_time = datetime.now(timezone.utc).isoformat()
     parent = psutil.Process(parent)
     for p in chain(parent.children(recursive=True), [parent]):
         try:
-            print(data_json(p, log_current_time, benchmark_experiment_start), file=data_log)
+            print(data_json(p, log_current_time, benchmark_experiment_start, benchmark_script), file=data_log)
         except psutil.NoSuchProcess:
             pass
 
-async def collect_process_data(parent: int, data_log, benchmark_experiment_start):
+async def collect_process_data(parent: int, data_log, benchmark_experiment_start, benchmark_script):
     try:
-        write_process_data(parent, data_log, benchmark_experiment_start)
+        write_process_data(parent, data_log, benchmark_experiment_start, benchmark_script)
         while True:
             await asyncio.sleep(0.05)
-            write_process_data(parent, data_log, benchmark_experiment_start)
+            write_process_data(parent, data_log, benchmark_experiment_start, benchmark_script)
     except Exception as e:
-        print(e, type(e))
+        print(e, type(e), file=sys.stderr)
 
-async def run_and_collect(program, data_log: Path, mortem_log: Path, benchmark_experiment_start: Path):
+async def run_and_collect(
+    program, data_log: Path, mortem_log: Path, benchmark_experiment_start: Path, root: Path, benchmark_script: Path, 
+):
     start_time = time.perf_counter()
     process = await asyncio.create_subprocess_exec(*program)
     pid = process.pid
     with data_log.open('a') as stdout:
-        process_data = asyncio.create_task(collect_process_data(pid, stdout, benchmark_experiment_start))
+        process_data = asyncio.create_task(collect_process_data(pid, stdout, benchmark_experiment_start, benchmark_script))
         await process.wait()
         end_time = time.perf_counter() 
         process_data.cancel()
     with mortem_log.open('a') as mortem_log:
-        print(benchmark_experiment_start, pid, end_time - start_time, sep=',', file=mortem_log)
+        print(benchmark_script.relative_to(root), benchmark_experiment_start, pid, end_time - start_time, sep=',', file=mortem_log)
 
 async def main():
     program = sys.argv[1:]
@@ -91,11 +93,15 @@ async def main():
     data_log = Path(os.environ.get('BENCHMARK_PROCESS_LOG'))
     mortem_log = Path(os.environ.get('BENCHMARK_MORTEM_LOG'))
     benchmark_experiment_start = os.environ.get('BENCHMARK_EXPERIMENT_START')
+    benchmark_script = Path(os.environ.get('BENCHMARK_SCRIPT'))
+    root = Path(get_project_root())
     await run_and_collect(
         program=program, 
         data_log=data_log, 
         mortem_log=mortem_log,
         benchmark_experiment_start=benchmark_experiment_start,
+        root=root,
+        benchmark_script=benchmark_script,
     )
 
 asyncio.run(main())

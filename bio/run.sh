5b49928e4bfc5ab78f85589a1432a0d1a33150e7
Evangelos Lamprou <vagos@lamprou.xyz>
2025-01-03 19:52:26 +0200

    Update bio run.sh
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- bio/run.sh
+++ bio/run.sh
@@ -8,27 +8,6 @@ if [[ "$@" == *"--small"* ]]; then
     IN_NAME=input_small.txt
 fi
 
-cat ${IN_NAME}|while read s_line;
-  do
-    sample=$(echo $s_line |cut -d " " -f 2);
-    pop=$(echo $s_line |cut -f 1 -d " ");
-    link=$(echo $s_line |cut -f 3 -d " ");
-    ### correcting labeling of chromosomes so that all are 1,2,3.. instead of chr1,chr2 or chromosome1 etc
-    echo "Processing Sample $sample";
-    # uniform the chromosomes in the file due to inconsistencies
-    samtools view -H "${IN}/$sample".bam | sed -e 's/SN:\([0-9XY]\)/SN:chr\1/' -e 's/SN:MT/SN:chrM/' \
-      | samtools reheader - "${IN}/$sample".bam > "${OUT}/$sample"_corrected.bam ;
-    # create bai file 
-    samtools index -b "${OUT}/$sample"_corrected.bam ;
-    ### Isolating each relevant chromosome based on Gen_locs
-    cut -f 2 ./Gene_locs.txt |sort |uniq |while read chr;
-  do  
-    echo 'Isolating Chromosome '$chr' from sample '${OUT}/$sample',  ';
-    samtools view -b "${OUT}/$sample"_corrected.bam chr"$chr" > "${OUT}/$pop"_"$sample"_"$chr".bam ;
-    echo 'Indexing Sample '$pop'_'${OUT}/$sample' ';
-    samtools index -b "${OUT}/$pop"_"$sample"_"$chr".bam;
-  done;
-  #rm "${OUT}/$sample"_corrected.bam;
-  #rm "${OUT}/$sample"_corrected.bam.bai;
-  #rm "${OUT}/$sample".bam
-done;
+BENCHMARK_SHELL=${BENCHMARK_SHELL:-bash}
+
+"$BENCHMARK_SHELL" ./scripts/bio.sh "$IN" "$IN_NAME" "$OUT"

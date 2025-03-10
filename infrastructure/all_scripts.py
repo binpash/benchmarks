9155c0c54e4d3754b414e368f85b7e13c216e500
Geoka1 <geoka001@gmail.com>
2025-03-10 15:11:41 +0200

    changed infra scripts, csvs and jsons for vps-audit merge

--- infrastructure/all_scripts.py
+++ infrastructure/all_scripts.py
@@ -6,19 +6,12 @@ import json
 
 from project_root import get_project_root
 
-# TODO: deleteme after merging vps-audits
-benchmark_rename_map = {
-    'vps-audit-negate': 'vps-audit'
-}
-
 def get_all_scripts(
     scripts_file: Path = get_project_root() / 'infrastructure/data/script-globs.json'
 ) -> list[Path]:
     scripts = scripts_file.read_text()
     benchmark_data: dict[str, dict[str, any]] = json.loads(scripts)
-    # TODO: deleteme after merging vps-audits
-    for old, new in benchmark_rename_map.items():
-        benchmark_data[new]["scripts"] = benchmark_data[new]["scripts"] + benchmark_data.pop(old)["scripts"]
+
     return {
         benchmark_name: [
             script

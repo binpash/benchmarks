7316a04f9436bd628ed55eb802584e5948db5b23
Geoka1 <geoka001@gmail.com>
2025-04-10 03:23:38 +0300

    fix

--- nlp/input.sh
+++ nlp/input.sh
@@ -20,6 +20,41 @@ if [ ! -f ./exodus ]; then
     curl -sf https://atlas-group.cs.brown.edu/data/gutenberg/3/3/4/2/33420/33420-0.txt > exodus
 fi
 
+for arg in "$@"; do
+    if [[ "$arg" == "--small" ]]; then
+        if [ ! -e ./pg-min ]; then
+            mkdir pg-small
+            cd pg-small || exit 1
+            book_count=10
+
+            head -n $book_count ../book_links.txt | while IFS= read -r line
+            do
+                full_url="https://atlas-group.cs.brown.edu/data/gutenberg/${line}"
+                echo "Downloading $full_url"
+                wget -q "$full_url"
+            done
+            cd ..
+            exit 0
+        fi
+    elif [[ "$arg" == "--min" ]]; then
+        if [ ! -e ./pg-min ]; then
+            mkdir pg-min
+            cd pg-min || exit 1
+            book_count=10
+
+            head -n $book_count ../book_links.txt | while IFS= read -r line
+            do
+                full_url="https://atlas-group.cs.brown.edu/data/gutenberg/${line}"
+                echo "Downloading $full_url"
+                wget -q "$full_url"
+            done
+
+            cd ..
+            exit 0
+        fi
+    fi
+done
+
 if [ ! -e ./pg ]; then
     mkdir pg
     cd pg || exit 1
@@ -35,32 +70,4 @@ if [ ! -e ./pg ]; then
     cd ..
 fi
 
-if [ ! -e ./pg-small ]; then
-    mkdir pg-small
-    cd pg-small || exit 1
-    book_count=10
 
-    head -n $book_count ../book_links.txt | while IFS= read -r line
-    do
-        full_url="https://atlas-group.cs.brown.edu/data/gutenberg/${line}"
-        echo "Downloading $full_url"
-        wget -q "$full_url"
-    done
-
-    cd ..
-fi
-
-if [ ! -e ./pg-min ]; then
-    mkdir pg-min
-    cd pg-min || exit 1
-    book_count=1
-
-    head -n $book_count ../book_links.txt | while IFS= read -r line
-    do
-        full_url="https://atlas-group.cs.brown.edu/data/gutenberg/${line}"
-        echo "Downloading $full_url"
-        wget -q "$full_url"
-    done
-
-    cd ..
-fi

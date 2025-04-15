f89bf6159ff549ac86a36aaf15272afbf472b3e4
Geoka1 <geoka001@gmail.com>
2025-04-15 17:47:52 +0300

    added min inputs file enc

--- file-enc/input.sh
+++ file-enc/input.sh
@@ -9,6 +9,18 @@ mkdir -p "$input_dir"
 DATA_LINK="https://atlas-group.cs.brown.edu/data/pcaps.zip"
 ZIP_DST="$input_dir/pcaps.zip"
 
+for arg in "$@"; do
+    if [ "$arg" = "--min" ]; then
+        mkdir -p "$input_dir/pcaps"
+        cp min_inputs/* "$input_dir/pcaps/"
+        exit 0
+    fi
+    if [ "$arg" = "--small" ]; then
+        # curl --insecure 'https://atlas-group.cs.brown.edu/data/file-enc/in_small.csv.gz' | gunzip > "$input_dir/in_small.csv"
+        continue
+    fi
+done
+
 if [ ! -d "$input_dir/pcaps" ]; then
     wget --no-check-certificate "$DATA_LINK" -O "$ZIP_DST"
     unzip "$ZIP_DST" -d "$input_dir"

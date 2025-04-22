29787265c6dad14f9788c98444e74fec882f8e08
Geoka1 <geoka001@gmail.com>
2025-04-22 21:12:33 +0300

    renamed image annotation

--- image_annotation/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BENCHMARK_SHELL=${BENCHMARK_SHELL:-bash}
-REPO_TOP=$(git rev-parse --show-toplevel)
-eval_dir="$REPO_TOP/image_annotation"
-inputs_dir="$eval_dir/inputs"
-outputs_dir="$eval_dir/outputs"
-rm -r "$inputs_dir" "$outputs_dir"

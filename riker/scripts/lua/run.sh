947eca8bf6d9fdda5f83c78244bf0c09068f08d0
Geoka1 <geoka001@gmail.com>
2025-04-24 21:06:01 +0300

    added autoconf

--- riker/scripts/lua/run.sh
+++ riker/scripts/lua/run.sh
@@ -7,5 +7,5 @@ scripts_dir="${eval_dir}/scripts"
 
 BENCHMARK_SHELL=${BENCHMARK_SHELL:-bash}
 export BENCHMARK_SCRIPT="$(realpath "$scripts_dir/lua/build.sh")"
-(cd "$input_dir/lua/lua-5.4.3/src" && $BENCHMARK_SHELL "$scripts_dir/lua/build.sh")
+(cd "$input_dir/lua/dev/src" && $BENCHMARK_SHELL "$scripts_dir/lua/build.sh")
 

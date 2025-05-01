93fbc2a9b66a0501179e01d1236d259395883c25
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-01 21:47:14 +0300

    Add small inputs to log-analysis
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- log-analysis/input.sh
+++ log-analysis/input.sh
@@ -2,14 +2,11 @@
 
 set -e
 
-# creates input/pcaps and input/nginx-logs
-
 REPO_TOP=$(git rev-parse --show-toplevel)
 eval_dir="${REPO_TOP}/log-analysis"
 input_dir="${eval_dir}/inputs"
 
 mkdir -p "$input_dir"
-#TODO add small and min versions
 
 for arg in "$@"; do
     if [[ "$arg" == "--min" ]]; then
@@ -23,18 +20,41 @@ for arg in "$@"; do
     fi
 done
 
-if [[ -d "$input_dir/nginx-logs" ]] && [[ -d "$input_dir/pcaps" ]]; then
-    echo "Data already downloaded and extracted."
-    exit 0
-fi
-url=https://atlas-group.cs.brown.edu/data/pcaps.zip
+for arg in "$@"; do
+    if [[ "$arg" == "--small" ]]; then
+        url=https://atlas-group.cs.brown.edu/data/pcaps.zip
+        zip_dst="$input_dir/pcaps.zip"
+        wget --no-check-certificate $url -O "$zip_dst"
+        unzip "$zip_dst" -d "$input_dir"
+        rm "$zip_dst"
+
+        url=https://atlas-group.cs.brown.edu/data/nginx.zip
+        zip_dst="$input_dir/nginx.zip"
+        wget --no-check-certificate $url -O "$zip_dst"
+        unzip "$zip_dst" -d "$input_dir"
+        rm "$zip_dst"
+
+        exit 0
+    fi
+done
+
+url=https://atlas-group.cs.brown.edu/data
+
 zip_dst="$input_dir/pcaps.zip"
-wget --no-check-certificate $url -O "$zip_dst"
+wget --no-check-certificate "$url"/pcaps.zip -O "$zip_dst"
 unzip "$zip_dst" -d "$input_dir"
 rm "$zip_dst"
 
-url=https://atlas-group.cs.brown.edu/data/nginx.zip
+wget --no-check-certificate "$url"/pcaps_large.zip -O "$zip_dst"
+unzip "$zip_dst" -d "$input_dir"/pcaps
+rm "$zip_dst"
+
 zip_dst="$input_dir/nginx.zip"
-wget --no-check-certificate $url -O "$zip_dst"
+wget --no-check-certificate "$url"/nginx.zip -O "$zip_dst"
 unzip "$zip_dst" -d "$input_dir"
 rm "$zip_dst"
+
+# TODO: Fix the large nginx download
+# wget --no-check-certificate "$url"/nginx_large.zip -O "$zip_dst"
+# unzip "$zip_dst" -d "$input_dir/nginx"
+# rm "$zip_dst"

388369eb13e04150ecf8b2e81af7b8b50a99bbf7
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-03 16:45:37 +0300

    cleanup covid-mts input.sh
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- covid-mts/input.sh
+++ covid-mts/input.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-REPO_TOP=$(git rev-parse --show-toplevel)
-eval_dir="${REPO_TOP}/covid-mts"
+TOP=$(git rev-parse --show-toplevel)
+eval_dir="${TOP}/covid-mts"
 input_dir="${eval_dir}/inputs"
+URL='https://atlas.cs.brown.edu/data/'
 
 mkdir -p "$input_dir"
 
+size=full
 for arg in "$@"; do
-    if [ "$arg" = "--min" ]; then
-        cp min_inputs/in_min.csv "$input_dir/in_min.csv"
+    case "$arg" in
+        --small) size=small ;;
+        --min)   size=min ;;
+    esac
+done
+
+if [ "$size" = "min" ]; then
+    cp min_inputs/* "$input_dir/"
+    exit 0
+fi
+
+if [ "$size" = "small" ]; then
+    if [ -f "$input_dir/in_small.csv" ]; then
         exit 0
     fi
-    if [ "$arg" = "--small" ]; then
-        if [ ! -f "$input_dir/in_small.csv" ]; then
-            curl --insecure 'https://atlas-group.cs.brown.edu/data/covid-mts/in_small.csv.gz' | gunzip > "$input_dir/in_small.csv"
-            exit 0
-        else
-            exit 0
-        fi
-    fi
-done
+    curl --insecure "$URL"/covid-mts/in_small.csv.gz | gunzip > "$input_dir/in_small.csv"
+fi
 
 if [ -f "$input_dir/in.csv" ]; then
     exit 0
 fi
+
 curl --insecure 'https://atlas-group.cs.brown.edu/data/covid-mts/in_full.csv.gz' | gunzip > "$input_dir/in.csv"

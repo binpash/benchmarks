125dad87384cbfc46da7b0afbab7e4b6e7b00720
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-03 16:49:27 +0300

    cleanup file-enc input.sh
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- file-enc/input.sh
+++ file-enc/input.sh
@@ -6,31 +6,34 @@ input_dir="${eval_dir}/inputs"
 
 mkdir -p "$input_dir"
 
-DATA_LINK="https://atlas-group.cs.brown.edu/data"
+URL="https://atlas-group.cs.brown.edu/data"
 ZIP_DST="$input_dir/pcaps.zip"
 
+size=full
 for arg in "$@"; do
-    if [ "$arg" = "--min" ]; then
-        mkdir -p "$input_dir/pcaps"
-        cp min_inputs/* "$input_dir/pcaps/"
-        exit 0
-    fi
-    if [ "$arg" = "--small" ]; then
-        wget --no-check-certificate $DATA_LINK/pcaps.zip -O "$input_dir/pcaps.zip"
-        unzip "$input_dir/pcaps.zip" -d "$input_dir"
-        rm "$input_dir/pcaps.zip"
-        exit 0
-    fi
+    case "$arg" in
+        --small) size=small ;;
+        --min)   size=min ;;
+    esac
 done
 
-if [ ! -d "$input_dir/pcaps" ]; then
-    wget --no-check-certificate $DATA_LINK/pcaps.zip -O "$input_dir/pcaps.zip"
+if [ "$size" = "min" ]; then
+    mkdir -p "$input_dir/pcaps"
+    cp min_inputs/* "$input_dir/pcaps/"
+    exit 0
+fi
+
+if [ "$size" = "small" ]; then
+    wget --no-check-certificate $URL/pcaps.zip -O "$input_dir/pcaps.zip"
     unzip "$input_dir/pcaps.zip" -d "$input_dir"
     rm "$input_dir/pcaps.zip"
-
-    wget --no-check-certificate "$DATA_LINK"/pcaps_large.zip -O "$ZIP_DST"
-    unzip "$ZIP_DST" -d "$input_dir"/pcaps
-    rm "$ZIP_DST"
-else
-    echo "pcaps already exists. Skipping download."
+    exit 0
 fi
+
+wget --no-check-certificate $URL/pcaps.zip -O "$input_dir/pcaps.zip"
+unzip "$input_dir/pcaps.zip" -d "$input_dir"
+rm "$input_dir/pcaps.zip"
+
+wget --no-check-certificate "$URL"/pcaps_large.zip -O "$ZIP_DST"
+unzip "$ZIP_DST" -d "$input_dir"/pcaps
+rm "$ZIP_DST"

0572b5e1934adb32dd7de3c12f732e4df08b1c28
Geoka1 <geoka001@gmail.com>
2025-05-03 17:25:57 +0300

    fixed bio inputs, input.sh, and added small hashes

--- bio/input.sh
+++ bio/input.sh
@@ -12,18 +12,36 @@ for arg in "$@"; do
     esac
 done
 
-mkdir -p "${IN}" outputs
+mkdir -p "$IN" outputs
 
 if [[ "$IN_NAME" == "input_min.txt" ]]; then
-    cp min_inputs/* "$IN/"
-    exit 0;
+    if [[ -d min_inputs ]]; then
+        cp min_inputs/* "$IN/"
+    else
+        echo "Directory 'min_inputs' not found." >&2
+        exit 1
+    fi
+    exit 0
+fi
+
+if [[ ! -f "$IN_NAME" ]]; then
+    echo "Input file '$IN_NAME' not found." >&2
+    exit 1
 fi
 
 while IFS= read -r s_line; do
     sample=$(echo "$s_line" | cut -d ' ' -f 2)
-    if [[ ! -f "$IN/$sample.bam" ]]; then
-        pop=$(echo "$s_line" | cut -d ' ' -f 1)
-        link=$(echo "$s_line" | cut -d ' ' -f 3)
-        wget -O "${IN}/$sample.bam" "$link"
+    link=$(echo "$s_line" | cut -d ' ' -f 3)
+
+    out_file="$IN/$sample.bam"
+
+    if [[ ! -f "$out_file" ]]; then
+        tmp_file="${out_file}.tmp"
+        if wget -O "$tmp_file" --no-check-certificate "$link"; then
+            mv "$tmp_file" "$out_file"
+        else
+            echo "Failed to download: $link" >&2
+            rm -f "$tmp_file"
+        fi
     fi
 done < "$IN_NAME"

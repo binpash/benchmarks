39b436c5013f8dd3b82b9438bd54211a155af708
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-04 19:20:50 +0300

    fix unix50 todo
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- unix50/fetch.sh
+++ unix50/fetch.sh
@@ -9,6 +9,14 @@ cd "$input_dir" || exit 1
 
 inputs=(1 2 3 4 5 6 7 8 9.1 9.2 9.3 9.4 9.5 9.6 9.7 9.8 9.9 10 11 12)
 
+size=full
+for arg in "$@"; do
+    case "$arg" in
+        --small) size=small ;;
+        --min)   size=min ;;
+    esac
+done
+
 for input in "${inputs[@]}"
 do
     if [ ! -f "${input}.txt" ]; then
@@ -16,34 +24,20 @@ do
     fi
 
     # Skip the 1M file if the --min flag is present
-    for arg in "$@"; do
-        if [ "$arg" = "--min" ]; then
-            continue 2
-        fi
-    done
+    if [ "$size" = "min" ]; then
+        continue
+    fi
 
-    # TODO: Maybe upload 1M and 3G files to the server?
-    if [ ! -f "${input}_1M.txt" ]; then
-        file_content_size=$(wc -c < "${input}.txt")
-        iteration_limit=$((1048576 / $file_content_size))
-        for (( i = 0; i < iteration_limit; i++ )); do
-            cat "${input}.txt" >> "${input}_1M.txt"
-        done
-        for (( i = 0; i < 30; i++ )); do
-            cat "${input}_1M.txt" >> "${input}_30M.txt"
-        done
+    if [ ! -f "${input}_30M.txt" ]; then
+        wget --no-check-certificate "${URL}/unix50/small/${input}_30M.txt" || exit 1
     fi
 
     # Skip the 3G file if the --small flag is present
-    for arg in "$@"; do
-        if [ "$arg" = "--small" ]; then
-            continue 2
-        fi
-    done
+    if [ "$size" = "small" ]; then
+        continue
+    fi
 
     if [ ! -f "${input}_3G.txt" ]; then
-        for (( i = 0; i < 3000; i++ )); do
-            cat "${input}_1M.txt" >> "${input}_3G.txt"
-        done
+        wget --no-check-certificate "${URL}/unix50/large/${input}_3G.txt" || exit 1
     fi
 done

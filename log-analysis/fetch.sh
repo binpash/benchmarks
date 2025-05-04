5919ed2c3733196cef4957ddddd5c41bce19947a
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-04 19:58:10 +0300

    fix log-analysis TODO
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- log-analysis/fetch.sh
+++ log-analysis/fetch.sh
@@ -5,17 +5,17 @@ set -e
 REPO_TOP=$(git rev-parse --show-toplevel)
 eval_dir="${REPO_TOP}/log-analysis"
 input_dir="${eval_dir}/inputs"
-url_prefix=https://atlas-group.cs.brown.edu/data
+URL=https://atlas-group.cs.brown.edu/data
 mkdir -p "$input_dir"
 
 size=full
 for arg in "$@"; do
-    if [[ "$arg" == "--min" ]]; then
-        size="min"
-    elif [[ "$arg" == "--small" ]]; then
-        size="small"
-    fi
+    case "$arg" in
+        --small) size=small ;;
+        --min)   size=min ;;
+    esac
 done
+
 
 if [[ ! -d "$input_dir/nginx-logs_$size" ]] || [[ ! -d "$input_dir/pcaps_$size" ]]; then
     if [[ "$size" == "min" ]]; then
@@ -25,13 +25,13 @@ if [[ ! -d "$input_dir/nginx-logs_$size" ]] || [[ ! -d "$input_dir/pcaps_$size" 
     fi
 
     if [[ "$size" == "small" ]]; then
-        wget --no-check-certificate $url_prefix/pcaps.zip -O "$input_dir/pcaps_$size.zip"
+        wget --no-check-certificate $URL/pcaps.zip -O "$input_dir/pcaps_$size.zip"
         unzip "$input_dir/pcaps_$size.zip" -d "$input_dir"
         mv "$input_dir/pcaps" "$input_dir/pcaps_$size"
         rm "$input_dir/pcaps_$size.zip"
 
         zip_dst="$input_dir/nginx.zip"
-        wget --no-check-certificate $url_prefix/nginx.zip -O "$zip_dst"
+        wget --no-check-certificate $URL/nginx.zip -O "$zip_dst"
         unzip "$zip_dst" -d "$input_dir"
         mv "$input_dir/nginx-logs" "$input_dir/nginx-logs_$size"
         rm "$zip_dst"
@@ -39,26 +39,27 @@ if [[ ! -d "$input_dir/nginx-logs_$size" ]] || [[ ! -d "$input_dir/pcaps_$size" 
     fi
 
     zip_dst="$input_dir/pcaps.zip"
-    wget --no-check-certificate "$url_prefix"/pcaps.zip -O "$zip_dst"
+    wget --no-check-certificate "$URL"/pcaps.zip -O "$zip_dst"
     unzip "$zip_dst" -d "$input_dir"
     mv "$input_dir/pcaps/" "$input_dir/pcaps_$size/"
     rm "$zip_dst"
 
-    wget --no-check-certificate "$url_prefix"/pcaps_large.zip -O "$zip_dst"
+    wget --no-check-certificate "$URL"/pcaps_large.zip -O "$zip_dst"
     unzip "$zip_dst" -d "$input_dir"/pcaps_$size
     rm "$zip_dst"
 
     zip_dst="$input_dir/nginx.zip"
-    wget --no-check-certificate $url_prefix/nginx.zip -O "$zip_dst"
+    wget --no-check-certificate $URL/nginx.zip -O "$zip_dst"
     unzip "$zip_dst" -d "$input_dir"
     mv "$input_dir/nginx-logs" "$input_dir/nginx-logs_$size"
     rm "$zip_dst"
 
+    zip_dst="$input_dir/nginx_large.zip"
+    wget --no-check-certificate "$URL"/log-analysis/web-server-access-logs.zip -O "$zip_dst"
+    unzip "$zip_dst" -d "$input_dir"
+    mv "$input_dir/access.log" "$input_dir/nginx-logs_$size/access.log"
+    rm "$zip_dst"
 else
     echo "Data already downloaded and extracted."
     exit 0
 fi
-# TODO: Fix the large nginx download
-# wget --no-check-certificate "$url"/nginx_large.zip -O "$zip_dst"
-# unzip "$zip_dst" -d "$input_dir/nginx"
-# rm "$zip_dst"

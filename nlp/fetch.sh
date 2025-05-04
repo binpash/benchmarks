91ff70ddf94d4926e5331b101aadcd2e522bbeed
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-04 20:36:33 +0300

    fix nlp fetch
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- nlp/fetch.sh
+++ nlp/fetch.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-cd "$(realpath "$(dirname "$0")")" || exit 1
-mkdir -p inputs
-cd inputs || exit 1
+TOP=$(git rev-parse --show-toplevel)
+URL="https://atlas-group.cs.brown.edu/data"
+
+input_dir="${TOP}/nlp/inputs"
+mkdir -p "$input_dir"
+cd "$input_dir" || exit 1
+
+size=full
+for arg in "$@"; do
+    case "$arg" in
+        --small) size=small ;;
+        --min)   size=min ;;
+    esac
+done
 
 if [ ! -f ./book_links.txt ]; then
-    wget -O book_links.txt "https://atlas-group.cs.brown.edu/data/gutenberg/books.txt"
+    wget -O book_links.txt "${URL}/gutenberg/books.txt"
     if [ ! -f book_links.txt ]; then
         echo "Failed to download book_links.txt"
         exit 1
@@ -13,47 +24,45 @@ if [ ! -f ./book_links.txt ]; then
 fi
 
 if [ ! -f ./genesis ]; then
-    curl -sf https://atlas-group.cs.brown.edu/data/gutenberg/8/0/0/8001/8001.txt > genesis
+    curl -sf ${URL}/gutenberg/8/0/0/8001/8001.txt > genesis
 fi 
 
 if [ ! -f ./exodus ]; then
-    curl -sf https://atlas-group.cs.brown.edu/data/gutenberg/3/3/4/2/33420/33420-0.txt > exodus
+    curl -sf ${URL}/gutenberg/3/3/4/2/33420/33420-0.txt > exodus
 fi
 
-for arg in "$@"; do
-    if [[ "$arg" == "--small" ]]; then
-        if [ ! -e ./pg-small ]; then
-            mkdir pg-small
-            cd pg-small || exit 1
-            book_count=3000
+if [[ "$size" == "small" ]]; then
+    if [ ! -e ./pg-small ]; then
+        mkdir pg-small
+        cd pg-small || exit 1
+        book_count=3000
 
-            head -n $book_count ../book_links.txt | while IFS= read -r line
-            do
-                full_url="https://atlas-group.cs.brown.edu/data/gutenberg/${line}"
-                echo "Downloading $full_url"
-                wget -q "$full_url"
-            done
-            cd ..
-            exit 0
-        fi
-    elif [[ "$arg" == "--min" ]]; then
-        if [ ! -e ./pg-min ]; then
-            mkdir pg-min
-            cd pg-min || exit 1
-            book_count=1
-
-            head -n $book_count ../book_links.txt | while IFS= read -r line
-            do
-                full_url="https://atlas-group.cs.brown.edu/data/gutenberg/${line}"
-                echo "Downloading $full_url"
-                wget -q "$full_url"
-            done
-
-            cd ..
-            exit 0
-        fi
+        head -n $book_count ../book_links.txt | while IFS= read -r line
+        do
+            full_url="${URL}/gutenberg/${line}"
+            echo "Downloading $full_url"
+            wget -q "$full_url"
+        done
+        cd ..
+        exit 0
     fi
-done
+elif [[ "$size" == "min" ]]; then
+    if [ ! -e ./pg-min ]; then
+        mkdir pg-min
+        cd pg-min || exit 1
+        book_count=1
+
+        head -n $book_count ../book_links.txt | while IFS= read -r line
+        do
+            full_url="${URL}/gutenberg/${line}"
+            echo "Downloading $full_url"
+            wget -q "$full_url"
+        done
+
+        cd ..
+        exit 0
+    fi
+fi
 
 if [ ! -e ./pg ]; then
     mkdir pg
@@ -61,7 +70,7 @@ if [ ! -e ./pg ]; then
 
     while IFS= read -r line
     do
-        full_url="https://atlas-group.cs.brown.edu/data/gutenberg/${line}"
+        full_url="${URL}/gutenberg/${line}"
         echo "Downloading $full_url"
         wget -q "$full_url"
     done < ../book_links.txt

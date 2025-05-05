7e98ad01e331285ef9ac8b67b8d90a2b9afec835
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-05 04:14:18 +0300

    improve kick tires
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- kick-tires.sh
+++ kick-tires.sh
@@ -2,12 +2,23 @@
 
 benchmarks="oneliners bio vps-audit file-enc log-analysis web-index media-conv covid-mts sklearn nlp aurpkg unix50 makeself riker max-temp"
 
-echo "[*] Running kick tires script"
-echo "[*] Running ./main.sh --min for each benchmark"
+log() { echo -e "[*] $1"; }
+
+log "Running kick tires script"
+
+log "Checking if user can run sudo"
+# Check if user can run sudo
+if ! sudo -v &> /dev/null; then
+  echo "You need to have sudo privileges to run this script (package installations will be required)."
+  exit 1
+fi
+
+
+log "Running ./main.sh --min for each benchmark"
 
 # STEP 1: Run ./main.sh --min in each benchmark directory
 for bench in $benchmarks; do
-  echo "Running benchmark: $bench"
+  log "Running benchmark: $bench"
   ./main.sh --min "$bench" || {
     echo "Failed to run --min for $bench"
     exit 1

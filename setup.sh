793133f00b1015437944ddcfe4860c9c838330f5
Geoka1 <geoka001@gmail.com>
2025-05-05 23:33:53 +0300

    fix in setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(realpath "$(dirname "$0")")" || exit 1
 
+REPO_TOP=$(git rev-parse --show-toplevel)
 sudo apt-get install -y autoconf automake libtool build-essential cloc time gawk python3 python3-pip python3-venv
 python3 -m venv venv
 source venv/bin/activate

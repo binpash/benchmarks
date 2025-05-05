069591ec5ee0aa9801affb453b5fa1cdf15a6160
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-05 22:40:59 +0300

    make oneliners install packages once
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- oneliners/install.sh
+++ oneliners/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+pkgs="wget bsdmainutils file dos2unix"
+
 sudo apt update
-sudo apt install -y \
-    wget \
-    bsdmainutils \
-    file \
-    dos2unix
+
+for pkg in $pkgs; do
+    if ! dpkg -s "$pkg" >/dev/null 2>&1; then
+        echo "Installing package: $pkg"
+        sudo apt-get install -y "$pkg"
+    fi
+done

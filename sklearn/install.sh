ad44ebfd7a52a99510a19432e15535fc6690e5d8
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-06 22:34:18 -0400

    make sklearn install detect older pip version

--- sklearn/install.sh
+++ sklearn/install.sh
@@ -5,4 +5,12 @@ sudo apt-get update
 TOP=$(git rev-parse --show-toplevel)
 cd "$TOP"/sklearn || exit 1
 
+# check if recent-enough pip version
+if pip install --help | grep -q -- '--break-system-packages'; then
+    true
+else
+    # upgrade pip
+    python3 -m pip install --upgrade pip --user >/dev/null 2>&1
+fi
+
 pip install -r requirements.txt --break-system-packages

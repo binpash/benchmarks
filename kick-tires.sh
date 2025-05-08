ac87ac16899a65483f026de217934fea12108cf6
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-08 16:23:27 +0300

    make kick tires work in container
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- kick-tires.sh
+++ kick-tires.sh
@@ -7,9 +7,8 @@ log() { echo -e "[*] $1"; }
 log "Running kick tires script"
 
 log "Checking if user can run sudo"
-# Check if user can run sudo
-if ! sudo -v &> /dev/null; then
-  echo "You need to have sudo privileges to run this script (package installations will be required)."
+if [[ $EUID -ne 0 ]] && ! sudo -v &> /dev/null; then
+  echo "You need to have sudo privileges or be the root user to run this script (package installations will be required)."
   exit 1
 fi
 

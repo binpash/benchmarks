ed8657649cb2f0a15f70bd7f0b94a6820bbd8c5f
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-10 00:46:57 +0300

    add pca stuff
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

---
+++ infrastructure/make_pca_plot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from do_pca import perform_pca_and_plot
+import ast
+from project_root import get_project_root
+import sys
+from pathlib import Path
+
+OUTPUT = sys.argv[1] if len(sys.argv) > 1 else None
+
+# make OUTPUT absolute path
+if OUTPUT:
+    OUTPUT = Path(OUTPUT).absolute()
+else:
+    OUTPUT = Path(__file__).parent / 'pca-row-plot.pdf'
+
+root = get_project_root()
+
+
+embedding_df = pd.read_csv(root / 'infrastructure/data/embeddings.csv')
+embedding_df['embedding'] = embedding_df['embedding'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+
+# Embedding is a list of numbers, turn them into columns
+embedding_df = pd.concat([embedding_df['benchmark'], embedding_df['embedding'].apply(pd.Series)], axis=1)
+
+big_bench = embedding_df
+
+perform_pca_and_plot(big_bench, embedding_df, 'dual-analysis')
---
+++ infrastructure/pc-analysis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TOP=$(git rev-parse --show-toplevel)
+cd "$TOP"/infrastructure || exit 1
+OUTPUT="$1"
+
+python3 ./make_pca_plot.py "$OUTPUT"

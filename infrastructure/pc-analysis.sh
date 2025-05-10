a44335d0848c64e09ee579b39ae1094b4146db4a
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-10 06:44:22 +0300

    make plot output nicer
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- infrastructure/pc-analysis.sh
+++ infrastructure/pc-analysis.sh
@@ -4,4 +4,9 @@ TOP=$(git rev-parse --show-toplevel)
 cd "$TOP"/infrastructure || exit 1
 OUTPUT="$1"
 
+[ -z "$OUTPUT" ] && {
+    echo "Usage: $0 <output_file>"
+    exit 1
+}
+
 python3 ./make_pca_plot.py "$OUTPUT"

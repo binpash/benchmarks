5f4ef8204c3be064f269e645f4a4077427e745f9
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-12 02:39:44 +0300

    try to make more deterministic
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- media-conv/scripts/to_mp3.sh
+++ media-conv/scripts/to_mp3.sh
@@ -9,6 +9,17 @@ mkdir -p $2
 
 for i in ~/*;
 do
-    out="$2/$(basename $i).mp3"
-    cat $i | ffmpeg -y -i pipe:0 -f mp3 -ab 192000 pipe:1 2>/dev/null > $out
+    out="$2/$(basename "$i").mp3"
+    cat "$i" |
+        ffmpeg -hide_banner -loglevel error \
+        -y -i pipe:0 \
+        -acodec libmp3lame -b:a 192k \
+        -ar 44100 -ac 2 \
+        -fflags +bitexact \
+        -flags:a +bitexact \
+        -write_xing 0 \
+        -id3v2_version 0 \
+        -map_metadata -1 \
+        -f mp3 pipe:1 \
+        > "$out" 2>/dev/null
 done

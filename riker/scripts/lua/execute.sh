79dfdc0369a6eb7161708c0364546a64ac460ca2
Geoka1 <geoka001@gmail.com>
2025-05-14 19:41:41 +0300

    riker fix

--- riker/scripts/lua/execute.sh
+++ riker/scripts/lua/execute.sh
@@ -7,5 +7,6 @@ scripts_dir="${eval_dir}/scripts"
 
 KOALA_SHELL=${KOALA_SHELL:-bash}
 export BENCHMARK_SCRIPT="$(realpath "$scripts_dir/lua/build.sh")"
+export BENCHMARK_INPUT_FILE="$(realpath "$input_dir/lua/dev/src")"
 (cd "$input_dir/lua/dev/src" && $KOALA_SHELL "$scripts_dir/lua/build.sh")
 

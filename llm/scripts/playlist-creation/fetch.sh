e173ec5587a835471f2cc12e665bf994143036c6
Geoka1 <geoka001@gmail.com>
2025-05-15 23:10:21 +0300

    updated size json and other logs

--- llm/scripts/playlist-creation/fetch.sh
+++ llm/scripts/playlist-creation/fetch.sh
@@ -40,7 +40,26 @@ elif [[ "$size" == "min" ]]; then
         echo "Data already downloaded and extracted."
         exit 0
     fi
-    cp -r "$REPO_TOP/llm/scripts/playlist-creation/min_inputs/"* "$input_dir/songs.min/"
+    data_url=https://os.unil.cloud.switch.ch/fma/fma_small.zip
+    zip_dst=$input_dir/fma_small.zip
+    out_dir=$input_dir
+    wget --no-check-certificate $data_url -O $zip_dst || {
+        echo "Failed to download $data_url"
+        exit 1
+    }
+    unzip -qq $zip_dst -d $out_dir || {
+        echo "Failed to unzip $zip_dst"
+        exit 1
+    }
+    rm "$zip_dst"
+    mv "$out_dir" "$input_dir/songs.min/"
+    # take out problematic files
+    cd "$input_dir/songs.min/" || exit 1
+    mkdir -p "$input_dir/keep"
+    mv $input_dir/songs.min/000 "$input_dir/keep/"
+    mv $input_dir/songs.min/001 "$input_dir/keep/"
+    rm -rf songs.min
+    mv "$input_dir/keep" "$input_dir/songs.min/"
     exit 0
 else
     if [[ -d "$input_dir/songs.full" ]]; then

3714d7c1983d0d811fe6602bdcc0f24ac7b1bcd2
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-19 01:04:44 +0300

    fix data dir
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- teraseq/scripts/PARAMS.sh
+++ teraseq/scripts/PARAMS.sh
@@ -11,8 +11,7 @@ CUR_DIR=$(pwd) # Current directory; useful for analyses
 DIR="$( cd "$TERASEQ_ROOT" && pwd )"
 INSTALL="$DIR/tools"
 SAMPLE_DIR="$DIR/inputs"
-DATA_DIR="$DIR/data"
-# DATA_DIR="$DIR/inputs/data"
+DATA_DIR="$DIR/inputs/data"
 RES_DIR="results"
 
 # Define common database filters

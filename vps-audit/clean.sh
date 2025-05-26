684077ed09737316e4e4cf2e67277b60666f850a
Geoka1 <geoka001@gmail.com>
2025-05-26 17:11:51 +0300

    updated clean script vps

--- vps-audit/clean.sh
+++ vps-audit/clean.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 REPO_TOP=$(git rev-parse --show-toplevel)
 eval_dir="${REPO_TOP}/vps-audit"
-rm "${eval_dir}"/*.txt
+rm "${eval_dir}"/vps-audit-negate-report.txt
+rm "${eval_dir}"/vps-audit-negate-processed.txt
+rm "${eval_dir}"/vps-audit-report.txt
+rm "${eval_dir}"/vps-audit-processed.txt

f2b5911b600d5fb29acbc1db135605a2e5e97708
Evangelos Lamprou <vagos@lamprou.xyz>
2025-05-27 01:34:48 +0300

    make templating work
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- infrastructure/web/make.sh
+++ infrastructure/web/make.sh
@@ -2,15 +2,25 @@
 set -euo pipefail
 set -x
 
-src="index.pd.html"
+src="html/index.html"
 dest="index.html"
+repo="binpash/benchmarks"
 
 header_content=$(<"header.html")
 footer_content=$(<"footer.html")
+updated="$(LANG=en_us_88591; date +'%R'; date +'%m/%d/%Y')"
+version=$(curl -s "https://api.github.com/repos/$repo/tags" | jq -r '.[0].name' || echo "0.1"); [ "$version" = "null" ] && version="0.1"
+revision=$(curl -s "https://api.github.com/repos/$repo/commits" | jq -r '.[0].sha' | cut -c1-7 || echo "unknown")
+commitmsg=$(curl -s "https://api.github.com/repos/$repo/commits/$revision" | jq -r '.commit.message' || echo "unknown")
 
 pandoc "$src"                         \
+  --standalone                        \
   --from html                         \
   --template "$src"                   \
   --to html5                          \
   --variable title="benchmarks.sh"    \
+  --variable version="$version"       \
+  --variable revision="$revision"     \
+  --variable updated="$updated"       \
+  --variable commitmsg="$commitmsg"   \
   --output "$dest"

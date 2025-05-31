94d839f5d82e2b3452c265ca011b8c7dbde7187b
Geoka1 <geoka001@gmail.com>
2025-05-31 17:52:48 +0300

    updated setup

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ cd "$(realpath "$(dirname "$0")")" || exit 1
 
 REPO_TOP=$(git rev-parse --show-toplevel)
 sudo apt-get update -y
-sudo apt-get install -y git autoconf automake libtool build-essential cloc time gawk strace lsof python3 python3-pip python3-venv
+sudo apt-get install -y git autoconf automake libtool build-essential cloc time gawk jq strace lsof python3 python3-pip python3-venv
 pip install --break-system-packages -r "$REPO_TOP/infrastructure/requirements.txt"

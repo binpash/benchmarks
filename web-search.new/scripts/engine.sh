872b2a72934b12ee07e63cc6199215bdf65f05c6
Geoka1 <geoka001@gmail.com>
2025-05-31 19:04:10 +0300

    updated web-index to work with syntactic analysis

--- web-search.new/scripts/engine.sh
+++ web-search.new/scripts/engine.sh
@@ -2,21 +2,17 @@
 # This is the main entry point of the search engine.
 cd "$(dirname "$0")" || exit 1
 
-while read -r url; do
+tail -f "${OUT}/urls.txt" | while read -r url; do
+  [ "$url" = stop ] && exit 0 # stop the engine if it sees the string "stop" 
 
-  if [[ "$url" == "stop" ]]; then
-    # stop the engine if it sees the string "stop" 
-    exit;
-  fi
 
   echo "[engine] crawling $url">/dev/stderr
   ./crawl.sh "$url" >${OUT}/content.txt
   echo "[engine] indexing $url">/dev/stderr
   ./index.sh ${OUT}/content.txt "$url"
 
-  if  [[ "$(cat ${OUT}/visited.txt | wc -l)" -ge "$(cat ${OUT}/urls.txt | wc -l)" ]]; then
-      # stop the engine if it has seen all available URLs
-      break;
+  if [ "$(cat "${OUT}/visited.txt" | wc -l)" -ge \
+       "$(cat "${OUT}/urls.txt"    | wc -l)" ]; then
+    break
   fi
-
-done < <(tail -f ${OUT}/urls.txt)
+done

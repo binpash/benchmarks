a0da179e3b280fcd1e852766dca5286b1306c40d
Geoka1 <geoka001@gmail.com>
2025-06-26 16:09:40 +0300

    fixed repl fetch

--- repl/fetch.sh
+++ repl/fetch.sh
@@ -2,6 +2,8 @@
 
 cd "$(dirname "$0")" || exit 1
 
+URL='https://atlas.cs.brown.edu/data/'
+
 eval_dir="$PWD"
 INPUT_DIR="${eval_dir}/inputs"
 CHROMIUM_DIR="${INPUT_DIR}/chromium"
@@ -10,10 +12,14 @@ COMMITS_DIR="${INPUT_DIR}/commits"
 mkdir -p "$CHROMIUM_DIR" "$COMMITS_DIR"
 
 if [ ! -d "$CHROMIUM_DIR/.git" ]; then
-    git clone https://chromium.googlesource.com/chromium/src.git "$CHROMIUM_DIR" || {
-        echo "Failed to clone Chromium repository"
+    wget -q "${URL}git/chromium.tar.gz" -O - | tar -xz -C "$INPUT_DIR"
+    if [ $? -ne 0 ]; then
+        echo "Failed to download or extract chromium data."
         exit 1
-    }
+    fi
+    echo "Chromium data downloaded and extracted to $CHROMIUM_DIR."
+else
+    echo "Chromium data already exists in $CHROMIUM_DIR."
 fi
 
 # No inputs required for the vps-audit scripts
